fb738c5a580df7d137bfadd30c69325daeff66c9
Jorge <hello@jhonboy.com>
2024-06-09 23:38:06 +0100

    🎨 STYLE - Keen Slider

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
 import Header from './components/Header.vue'
 import AboutUs from './components/AboutUs.vue'
 import Products from './components/Products.vue';
-import Footer from './components/Footer.vue';
 import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue';
 
 
 
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,50 +2,47 @@
   <section class="section-3" id="section-3">
     <div class="products">
       <h2>Productos</h2>
-      <div class="container-product-top">
-        <div class="container-nav">
+      <div class="container-nav">
           <button type="button" aria-label="flecha a la izquierda" id="button--left"><object id="arrow--left" type="image/svg+xml" data="/images/arrow_left.svg"></object></button>
           <button type="button" aria-label="flecha a la derecha" id="button--right"><object id="arrow--right" type="image/svg+xml" data="/images/arrow_right.svg"></object></button>
-        </div>
       </div>
-      <div class="container-slider">
         <div class="keen-slider" ref="sliderContainer">
-          <div class="keen-slider__slide number-slide1">
+          <div class="keen-slider__slide number-slide">
             <h3>Film</h3>
             <h4>Film manual y de máquina estirables. Tenemos diferentes preestiros y de material reciclado.</h4>
             <div class="svg-container-products">
               <img alt="ilustración de una caja siendo embalada con film transparente" src="/images/film.svg">
             </div>
           </div>
-          <div class="keen-slider__slide number-slide2">
+          <div class="keen-slider__slide number-slide">
             <h3>Precinto</h3>
             <h4>Acrílicos, base solvente y PVC de diferentes calidades, medidas y colores.</h4>
             <div class="svg-container-products">
               <img alt="ilustración de una caja y tres rollos de precinto al lado izquierdo" src="/images/Precinto.svg">
             </div>
           </div>
-          <div class="keen-slider__slide number-slide3">
+          <div class="keen-slider__slide number-slide">
             <h3>Big Bag</h3>
             <h4>Reutilizables de diferentes medidas, materiales, pesos y especificaciones.</h4>
             <div class="svg-container-products">
               <img alt="ilustración de un big bag lleno" src="/images/Bags.svg">
             </div>
           </div>
-          <div class="keen-slider__slide number-slide4">
+          <div class="keen-slider__slide number-slide">
             <h3>Fleje</h3>
             <h4>Polipropileno y reciclado para máquina y manual, en diferentes medidas.</h4>
             <div class="svg-container-products">
               <img alt="ilustración de una bobina de fleje" src="/images/Fleje.svg">
             </div>
           </div>
-          <div class="keen-slider__slide number-slide5">
+          <div class="keen-slider__slide number-slide">
             <h3>Poliolefina</h3>
             <h4>Bobinas semitubo de diferentes medidas y gramajes.</h4>
             <div class="svg-container-products">
               <img alt="ilustración de una bobina de poliolefina y una bandeja de comida embalada" src="/images/Poleolefina.svg">
             </div>
           </div>
-          <div class="keen-slider__slide number-slide6">
+          <div class="keen-slider__slide number-slide">
             <h3>Cordel</h3>
             <h4>Bobinas de cordel para máquinas industriales y manuales de tres cabos.</h4>
             <div class="svg-container-products">
@@ -54,7 +51,6 @@
           </div>
         </div>
       </div>
-    </div>
   </section>
 </template>
 <script>
