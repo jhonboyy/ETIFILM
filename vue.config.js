b431bc79120397c0aefb7eaa57ca80118ffe44a3
Jorge <hello@jhonboy.com>
2024-06-11 20:09:38 +0100

    🛠 BUILD - .env variables

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
+      // Asegúrate de no sobrescribir todo el objeto process.env
       Object.assign(definitions[0]['process.env'], {
         __VUE_OPTIONS_API__: JSON.stringify(true),
         __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
+        // Asegúrate de incluir manualmente las variables que necesitas, si no aparecen automáticamente
+        VUE_APP_RECAPTCHA_PUBLIC_KEY: JSON.stringify(process.env.VUE_APP_RECAPTCHA_PUBLIC_KEY),
       });
       return definitions;
     });
