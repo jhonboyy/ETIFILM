fcfeb7f6d1500e7a846ca9179c57e7dc11b68764
Jorge <hello@jhonboy.com>
2024-06-19 09:20:30 +0100

    🔧 SETUP - Node express server

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,38 @@
 module.exports = {
+  outputDir: 'dist',
   chainWebpack: config => {
-    config.plugin('define').tap(definitions => {
-      // Asegúrate de no sobrescribir todo el objeto process.env
-      Object.assign(definitions[0]['process.env'], {
-        __VUE_OPTIONS_API__: JSON.stringify(true),
-        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
-        // Asegúrate de incluir manualmente las variables que necesitas, si no aparecen automáticamente
-        VUE_APP_RECAPTCHA_PUBLIC_KEY: JSON.stringify(process.env.VUE_APP_RECAPTCHA_PUBLIC_KEY),
-      });
-      return definitions;
+    // Asegurarse de que las definiciones existan antes de intentar modificarlas
+    config.plugin('define').tap(args => {
+      let env = args[0]['process.env'] || {}; // Inicializa 'env' si no existe
+      env.__VUE_OPTIONS_API__ = JSON.stringify(true);
+      env.__VUE_PROD_DEVTOOLS__ = JSON.stringify(false);
+      env.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = JSON.stringify(true);
+      env.VUE_APP_RECAPTCHA_PUBLIC_KEY = JSON.stringify(process.env.VUE_APP_RECAPTCHA_PUBLIC_KEY);
+
+      args[0]['process.env'] = env;
+      return args;
     });
   },
   configureWebpack: {
     optimization: {
+      splitChunks: {
+        chunks: 'all'
+      },
       minimize: true
     }
+  },
+  pwa: {
+    name: 'Etifilm App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      background_color: '#FFFFFF'
+    }
+  },
+  productionSourceMap: false,
+  pluginOptions: {
+    // Aquí puedes agregar opciones adicionales para otros plugins que uses
   }
 }
