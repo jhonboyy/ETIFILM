742e6fddfc99504dc59280e615c73435a6df29fb
Jorge <hello@jhonboy.com>
2024-06-21 11:26:51 +0100

    🔧 SETUP - Vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/styles/styles.css';
+import {Analytics} from '@vercel/analytics';
 
+<Analytics />
 
 
 createApp(App).mount('#app');
-
